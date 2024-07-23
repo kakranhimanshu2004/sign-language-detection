f4c2464af87141783ab251d8a39096f8a3202c5c
kakranhimanshu2004 <kakranhimanshucu@mgial.com>
2024-07-23 18:38:13 +0530

    project uploaded

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'sign-language-detection',
+    version= '0.0.0',
+    author= 'Himanshu',
+    author_email= 'kakranhimanshucu@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
